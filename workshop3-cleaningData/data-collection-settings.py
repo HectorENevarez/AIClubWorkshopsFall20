70a82d23cc9f75299865d00618356a2bf5152a8a
Hector-Nevarez <44732159+Hector-Nevarez@users.noreply.github.com>
2020-06-11 19:27:49 -0700

    Sorted data cleaning into folder

--- workshop3-cleaningData/data-collection-settings.py
+++
@@ -1,8 +0,0 @@
-import data_collection as dc
-import pandas as pd 
-
-path = "C:/Users/Hneva/OneDrive/Desktop/Chrome-Driver/chromedriver.exe"
-
-df = dc.get_jobs('Software Engineer',1000, False, path, 15)
-
-df.to_csv('glassdoor_job.csv', index = False)
